2c4c391d59de3df6de6ab1b14c0d29f8c113bf39
cookiesncache <cookiesncache@gmail.com>
2023-01-29 22:38:28 -0500

    Change defaults

--- source/options-storage.js
+++ source/options-storage.js
@@ -2,11 +2,11 @@ import OptionsSync from 'webext-options-sync';
 
 export default new OptionsSync({
 	defaults: {
-		colorRed: 244,
-		colorGreen: 67,
-		colorBlue: 54,
+		colorRed: 0,
+		colorGreen: 0,
+		colorBlue: 0,
 		text: 'Set a text!',
-		rateRent: 0.5
+		rateRent: 0.5000
 	},
 	migrations: [
 		OptionsSync.migrations.removeUnused,
