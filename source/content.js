063f016d4b96ea321dec391b4021a2d065d0e7b0
cookiesncache <cookiesncache@gmail.com>
2023-01-29 21:27:09 -0500

    Update content.js

--- source/content.js
+++ source/content.js
@@ -1,11 +1,12 @@
 import optionsStorage from './options-storage.js';
 
 console.log('🟢 Content script loaded for', chrome.runtime.getManifest().name);
+
 async function init() {
 	const options = await optionsStorage.getAll();
 	const color = `rgb(${options.colorRed}, ${options.colorGreen}, ${options.colorBlue})`;
 	const notice = document.createElement('div');
-	
+
 	notice.id = 'text-notice';
 	notice.innerHTML = options.text;
 	notice.style.border = '2px solid ' + color;
