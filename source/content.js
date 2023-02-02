00d3d7c05236743d5a72c7a521355c834331be1a
cookiesncache <cookiesncache@gmail.com>
2023-02-01 23:47:43 -0500

    jQuery working!

--- source/content.js
+++ source/content.js
@@ -1,11 +1,14 @@
 import optionsStorage from './options-storage.js';
+import * as jQuery from "jquery";
 
-console.log('🟢 Content script loaded for', chrome.runtime.getManifest().name);
+const $ = jQuery.default;
+
+console.log('🦭 Content script loaded for', chrome.runtime.getManifest().name);
 
 async function init() {
 	const options = await optionsStorage.getAll();
-	
-	console.log(`Rent Rate: ${options.rateRent}, Other Rate: ${options.rateOther}, Parking Fee: ${options.feeParking}`);
+
+	console.log($( "#UnpaidDiv" ).index());
 }
 
 init();
--- source/options-storage.js
+++ source/options-storage.js
@@ -2,9 +2,12 @@ import OptionsSync from 'webext-options-sync';
 
 export default new OptionsSync({
 	defaults: {
+		enableParking: true,
+		descriptionParking: "Parking Income",
+		feeParking: 185,
+		descriptionRent: "Rent",
 		rateRent: 0.3333,
-		rateOther: 0.3333,
-		feeParking: 185
+		rateOther: 0.3333
 	},
 	migrations: [
 		OptionsSync.migrations.removeUnused,
