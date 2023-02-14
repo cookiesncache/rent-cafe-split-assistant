821352545c4ea66a8037bf0abd8f023cd5964dfc
cookiesncache <cookiesncache@gmail.com>
2023-02-13 23:57:26 -0500

    Add listener, remove run_at

--- source/background.js
+++ source/background.js
@@ -1,2 +1,14 @@
 // eslint-disable-next-line import/no-unassigned-import
 import './options-storage.js';
+
+const paymentPage = "https://*.securecafe.com/residentservices/*/paymentonetime.aspx";
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    if (tab.url.match(paymentPage)) {
+        chrome.tabs.executeScript(tab.id, {
+            "file": "content.js"
+        }, function () {
+            console.log('🦭 Content script executed for', chrome.runtime.getManifest().name);
+        });
+    }
+});
